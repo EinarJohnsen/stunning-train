b8388cb34e54d9c482e1c904f4862b30f6861c3e
EinarJohnsen <brann_einar@hotmail.com>
2016-04-05 22:10:38 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,52 +1,47 @@
 
-			var SomeComponent = React.createClass({
+var SomeComponent = React.createClass({
 
-				render: function(){
-					return(
-								<h3>{this.props.header}</h3>
-						);
-				}
-			});
+	render: function(){
+		return(
+			<h3>{this.props.header}</h3>
+			);
+		}
+	});
 
-			React.render( <SomeComponent header="Dette er en tittel :)"/>, document.getElementById('content'));
+	React.render( <SomeComponent header="Dette er en tittel :)"/>, document.getElementById('content'));
 
-			var EinarsComponent = React.createClass({
+var EinarsComponent = React.createClass({
+	getInitialState: function(){
+ 		return {
+    			post: ''
+  		};
+	},
+	componentDidMount: function(){
+		var x = Math.floor((Math.random() * 9000) + 999);
+		var y = x.toString();
+		$.ajax({
+			url: "https://api.bring.com/shippingguide/api/postalCode.html",
+			data: {clientUrl: "localhost", pnr: y },
+			success: function(data){
+				var s = $(data).html();
+				this.setState({post: s});
+			}.bind(this)	
+		});
+	},
 
+	render: function(){
+		return(			
+			<li><input className="mdl-checkbox__input" type="checkbox"></input>{this.state.post}</li>
+		);
+	}
+});
 
-				getInitialState: function(){
- 					 return {
-    						post: ''
-  						};
-					},
-
-				componentDidMount: function(){
-				var x = Math.floor((Math.random() * 9000) + 999);
-				var y = x.toString();
-
-				$.ajax({
-					url: "https://api.bring.com/shippingguide/api/postalCode.html",
-					data: {clientUrl: "localhost", pnr: y },
-					success: function(data){
-						var s = $(data).html();
-						this.setState({post: s});
-					}.bind(this)	
-				});
-				},
-
-				render: function(){
-					
-					return(			
-							<li><input className="mdl-checkbox__input" type="checkbox"></input>{this.state.post}</li>
-							);
-						}
-					});
-
-				React.render(<ul>
-							<EinarsComponent />
-						 	<EinarsComponent />
-						 	<EinarsComponent />
-						 	<EinarsComponent />
-						 	<EinarsComponent />
-							 </ul>
-							, document.getElementById('hei'));
+React.render(<ul>
+		<EinarsComponent />
+		<EinarsComponent />
+		<EinarsComponent />
+		<EinarsComponent />
+		<EinarsComponent />
+		 </ul>
+		, document.getElementById('hei'));
 	
