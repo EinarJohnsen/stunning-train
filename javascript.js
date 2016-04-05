6f969272c256438e707501cccda8f1d873123654
EinarJohnsen <brann_einar@hotmail.com>
2016-04-05 22:04:00 +0200

    Added files via upload

---
+++ javascript.js
@@ -0,0 +1,52 @@
+
+			var SomeComponent = React.createClass({
+
+				render: function(){
+					return(
+								<h3>{this.props.header}</h3>
+						);
+				}
+			});
+
+			React.render( <SomeComponent header="Dette er en tittel :)"/>, document.getElementById('content'));
+
+			var EinarsComponent = React.createClass({
+
+
+				getInitialState: function(){
+ 					 return {
+    						post: ''
+  						};
+					},
+
+				componentDidMount: function(){
+				var x = Math.floor((Math.random() * 9000) + 999);
+				var y = x.toString();
+
+				$.ajax({
+					url: "https://api.bring.com/shippingguide/api/postalCode.html",
+					data: {clientUrl: "localhost", pnr: y },
+					success: function(data){
+						var s = $(data).html();
+						this.setState({post: s});
+					}.bind(this)	
+				});
+				},
+
+				render: function(){
+					
+					return(			
+							<li><input className="mdl-checkbox__input" type="checkbox"></input>{this.state.post}</li>
+							);
+						}
+					});
+
+				React.render(<ul>
+							<EinarsComponent />
+						 	<EinarsComponent />
+						 	<EinarsComponent />
+						 	<EinarsComponent />
+						 	<EinarsComponent />
+							 </ul>
+							, document.getElementById('hei'));
+	
